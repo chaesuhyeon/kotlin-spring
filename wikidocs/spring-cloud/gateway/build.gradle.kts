8dbf29ebcf8757921a2cd8a5e620b01328c900af
chaesuhyeon <ddd8177@gmail.com>
2025-10-13 08:28:37 +0900

    🎉 위키독스 [스프링 클라우드와 서비스 메시] - 4장 : 게이트웨이 프로젝트 생성

---
+++ wikidocs/spring-cloud/gateway/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.5.6"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.ecommerce.gateway"
+version = "0.0.1-SNAPSHOT"
+description = "wikidocs example"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2025.0.0"
+
+dependencies {
+    // (내부에 spring-boot-starter-webflux, netty 포함 -> starter-web을 같이 넣어주면 충돌남)
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+
+    // 코루틴과 Reactor(WebFlux)를 함께 사용하기 위한 의존성
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
