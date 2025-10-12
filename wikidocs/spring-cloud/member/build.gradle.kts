39ce608785714cd88db7c43f48127070fac53132
chaesuhyeon <ddd8177@gmail.com>
2025-10-13 08:27:12 +0900

    ✨ 위키독스 [스프링 클라우드와 서비스 메시] - 3장 :  테스트 코드 템플릿 작성

--- wikidocs/spring-cloud/member/build.gradle.kts
+++ wikidocs/spring-cloud/member/build.gradle.kts
@@ -25,9 +25,18 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // ========== 테스트용 H2 데이터베이스 ==========
+    runtimeOnly("com.h2database:h2")
+
+    // ========== test ========== //
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.8.1") // Kotest Runner (JUnit5 위에서 동작)
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.8.1") // Kotest Assertions
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3") // Spring과 Kotest를 연동하기 위한 라이브러리
+    testImplementation("io.mockk:mockk:1.13.10") // 코틀린 네이티브 Mocking 라이브러리
 }
 
 kotlin {
