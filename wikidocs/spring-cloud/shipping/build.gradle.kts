4aadea8ac5963b3102b39f62176e1e6912e62f0c
chaesuhyeon <ddd8177@gmail.com>
2025-11-11 10:23:53 +0900

    🎉 shipping 프로젝트 생성

---
+++ wikidocs/spring-cloud/shipping/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.5.7"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.ecommerce"
+version = "0.0.1-SNAPSHOT"
+description = "wikidocs msa example project"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // Spring Cloud Stream 스타터
+    implementation("org.springframework.cloud:spring-cloud-stream")
+    // Kafka 바인더 추가
+    implementation("org.springframework.cloud:spring-cloud-stream-binder-kafka")
+}
+
+// Spring Cloud BOM 설정 필수
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.1")
+    }
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
